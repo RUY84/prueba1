9f0f537c4111cd03635af2c0035537e6015c8465
RUY84 <rodrigocardozo84@gmail.com>
2022-11-06 18:10:56 -0300

    probandoTad

---
+++ catalogo.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Programador web con PHP y MySQL</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css"/>
+
+</head>
+<body>
+	<section id="contenedor">
+		<header>
+			<nav id="botonera">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="catalogo.php">Catalogo0</a></li>
+					<li><a href="#">Noticias</a></li>
+					<li><a href="#">Clientes></a></li>
+					<li><a href="contacto.php">Contactenos</a></li>
+				</ul>
+			</nav>
+			<div id="marca">
+				<h1>Programador web con PHP y MySQL</h1>
+			</div>
+		</header>
+		<section id="contenido">
+			<h2>Catálogo</h2>
+		</section>
+		<footer>
+			Desarrollado por <a href="http.elearning-total.com" target="_new">Programador web con PHO y MySQL</a>
+		</footer>
+	</section>
+</body>
+</html>
